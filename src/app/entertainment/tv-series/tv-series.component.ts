254bbd6917a66ca8b3f1f66a02bdace26dd55406
mosesfuraha <moses.furaha@amalitechtraining.org>
2024-09-27 19:28:51 +0200

    implement search functionality on all pages

--- src/app/entertainment/tv-series/tv-series.component.ts
+++ src/app/entertainment/tv-series/tv-series.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, Observable } from 'rxjs';
 import { ContentItem } from '../models/data.interface';
 import {
   AppState,
   selectAllEntertainment,
 } from '../store/entertainment.reducers';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-tv-series',
@@ -17,28 +17,35 @@ export class TvSeriesComponent implements OnInit {
   @Input() searchTerm: string | null = '';
   tvSeriesContent$!: Observable<ContentItem[]>;
 
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>,
+    private searchService: SearchService
+  ) {}
 
   ngOnInit(): void {
     this.loadTvSeriesContent();
+
+    this.searchService
+      .getFilteredContent(this.tvSeriesContent$)
+      .subscribe((filteredContent) => {
+        this.tvSeriesContent$ = new Observable((observer) => {
+          observer.next(filteredContent);
+          observer.complete();
+        });
+      });
   }
 
   private loadTvSeriesContent(): void {
-    this.tvSeriesContent$ = this.store.select(selectAllEntertainment).pipe(
-      map((content: ContentItem[]) =>
-        content.filter(
-          (item) =>
-            item.category === 'TV Series' && // Filter to show only TV Series
-            (this.searchTerm
-              ? item.title
-                  .toLowerCase()
-                  .includes(this.searchTerm.toLowerCase()) ||
-                item.rating
-                  .toLowerCase()
-                  .includes(this.searchTerm.toLowerCase())
-              : true)
+    this.tvSeriesContent$ = this.store
+      .select(selectAllEntertainment)
+      .pipe(
+        map((content: ContentItem[]) =>
+          content.filter((item) => item.category === 'TV Series')
         )
-      )
-    );
+      );
+  }
+
+  onSearch(term: string): void {
+    this.searchService.search(term);
   }
 }
